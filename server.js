24a82c451a4fd05ee5f75c3091d7f7cac682fcea
t9nsamp <62615750+t9nsamp@users.noreply.github.com>
2020-04-13 01:58:30 +0700

    Update server.js

--- server.js
+++ server.js
@@ -25,27 +25,28 @@
 
     app.post('/webhook', line.middleware(config), (req, res) => {
         //var intent = data.queryResult.intent.displayName //Get Intent from Dialogflow
-        var userMsg = message.text; //Get message from user Line
+        var userMsg = req.body.events[0].message.text; //Get message from user Line
         if(req.body.events[0].type === 'message' && req.body.events[0].message.type === 'text'){
+
             postToDialogflow(req);
           }
+          
+        else if (req.body.events[0].type === 'message' && req.body.events[0].message.type === 'location'){ 
+
+      Promise
+        //.all(req.body.events.map(handleEvent))
+        .all(req.body.events.map(handleLocationEvent))
+        .then((result) => res.json(result))
+        .catch(err => console.log('err', err))
+
+        }
 
     });
-    if(req.body.events[0].type === 'message' && req.body.events[0].message.text === 'bank'){
-    }
-      else if (req.body.events[0].type === 'message' && req.body.events[0].message.type === 'location'){ 
-
-        Promise
-          //.all(req.body.events.map(handleEvent))
-          .all(req.body.events.map(handleLocationEvent))
-          .then((result) => res.json(result))
-          .catch(err => console.log('err', err))
-  
-          } 
+    
     function handleLocationEvent(event) {
       
       return new Promise((resolve, reject) => {
-        
+        if((userMsg != 'bank'){  
           var userlat = parseFloat(event.message.latitude)
           var userlng = parseFloat(event.message.longitude)
           const voltajson = peavolta.Sheet1
@@ -277,11 +278,10 @@
             //resolve(client.replyMessage(event.replyToken, msg))
             reject(msg)
           }
-        
+        } 
       }
       )
     }
-  }
 
     const postToDialogflow = req => {
         req.headers.host = "bots.dialogflow.com";
